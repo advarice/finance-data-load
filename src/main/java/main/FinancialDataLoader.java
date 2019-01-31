f6232299f1042685a47b62f3f2443fc6e0b833d5
yfc <chen.yaofeng@yahoo.com>
2019-01-31 07:21:01 -0500

    set apikey=placeholder

--- src/main/java/main/FinancialDataLoader.java
+++ src/main/java/main/FinancialDataLoader.java
@@ -29,14 +29,14 @@ public class FinancialDataLoader implements CommandLineRunner {
 
     @Override
     public void run(String... args){
-        FinancialDataDao dao = (FinancialDataDao)ctx.getBean("financialDataDao");
-        dao.testConnection();
+        //FinancialDataDao dao = (FinancialDataDao)ctx.getBean("financialDataDao");
+        //dao.testConnection();
 
-        RestTemplate rt=new RestTemplate();
-        WeeklyPrices wp=rt.getForObject("https://www.alphavantage.co/query?function=TIME_SERIES_WEEKLY&symbol=MSFT&apikey=demo", WeeklyPrices.class);
-        System.out.println(wp.toString());
+        //RestTemplate rt=new RestTemplate();
+        //WeeklyPrices wp=rt.getForObject("https://www.alphavantage.co/query?function=TIME_SERIES_WEEKLY&symbol=MSFT&apikey=demo", WeeklyPrices.class);
+        //System.out.println(wp.toString());
         //String s=(String)ctx.getBean("testString");
-        dao.insert(wp);
+        //dao.insert(wp);
         IAction buildRestUrlAction = (BuildRestUrlAction)ctx.getBean("buildRestUrlAction");
         IAction loadDataAction = (LoadDataAction)ctx.getBean("loadDataAction");
         buildRestUrlAction.action();
